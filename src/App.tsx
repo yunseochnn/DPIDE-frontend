665d885028a05531f6b57040e75ffb2d93637e87
yunseochnn <10cys10@gmail.com>
2024-09-25 15:03:02 +0900

    style: styles폴더 내용 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,23 @@ import Login from './pages/LoginPage/Login';
 import SignUp from './pages/SignUpPage/SignUp';
 import MyPage from './pages/MyPagePage/MyPage';
 import StartPage from './pages/StartPage/StartPage';
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/Theme';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/ide/:projectId" element={<Ide />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/start" element={<StartPage />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/ide/:projectId" element={<Ide />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
