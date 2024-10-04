357410464b96f3723a89692ef960812250d4473d
qowldud <bjy6631@daum.net>
2024-10-04 14:57:46 +0900

    feat: IDE 연동

--- src/pages/IDEPage/Editor/Code.tsx
+++ src/pages/IDEPage/Editor/Code.tsx
@@ -3,7 +3,7 @@ import { FaPlay } from 'react-icons/fa6';
 import { CodeButton, CodeButtons, CodeTop, CodeWrapper, Edit, Files } from './Code.style';
 import CodeEditor from './CodeEditor';
 import FileList from '../../../components/FileList/FileList';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import FileState from '../../../recoil/File/atoms';
 import FileSaveRequest from '../../../apis/IDE/File/FileSaveRequest';
 import { useParams } from 'react-router-dom';
@@ -12,12 +12,12 @@ import { useCookies } from 'react-cookie';
 import { toast } from 'react-toastify';
 
 const Code = () => {
-  const File = useRecoilValue(FileState);
+  const [File, setFile] = useRecoilState(FileState);
   const [cookies] = useCookies(['Authorization']);
   const Authorization = cookies['Authorization'];
   const { projectId } = useParams();
   const code = useRecoilValue(CodeState);
-  const codeId = Number(code);
+  const codeId = Number(code.id);
   const id = Number(projectId);
 
   const onSaveClickHandler = async () => {
@@ -25,6 +25,13 @@ const Code = () => {
       const response = await FileSaveRequest(id, codeId, code.content, Authorization);
 
       if (response.ok) {
+        const newFile = File.map(file => {
+          if (file.id === code.id) {
+            return { ...file, content: code.content, modifyContent: code.content };
+          }
+          return file;
+        });
+        setFile(newFile);
         toast.success('파일을 저장하였습니다.', {
           pauseOnHover: false,
           autoClose: 2000,
