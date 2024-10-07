28ae1db272e2aec8c77a1ab3c96280ebee0ee351
yunseochnn <10cys10@gmail.com>
2024-10-07 11:49:23 +0900

    design: 채팅 검색

--- src/pages/IDEPage/Ide.style.ts
+++ src/pages/IDEPage/Ide.style.ts
@@ -47,17 +47,3 @@ export const IdeChat = styled.div`
     flex: 350;
   }
 `;
-export const IdeChat_Top = styled.div`
-  width: 100%;
-  height: 40px;
-  border-bottom: 1px solid black;
-  display: flex;
-  align-items: center;
-  padding-left: 23px;
-
-  div {
-    font-size: 16px;
-    color: white;
-    font-weight: 700;
-  }
-`;
